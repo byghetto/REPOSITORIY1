482cff2c9d80bd7943ed57cc4c229047da80f58a
Byilia <tawerghetto2288@gmail.com>
2023-03-30 22:21:11 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+
+    }
+
+    public static void task1() {
+        int[] weights = new int[12];
+// Объявляем массив
+        weights[0] = 90;
+// Присваиваем значение 1-му элементу
+        int januaryWeight = weights[0];
+// Записываем значение 1-го элемента массива в переменную
+        System.out.println(januaryWeight);
+// Выводим в консоль 1-й элемент массива. Способ 1
+        System.out.println(weights[0]);
+// Выводим в консоль 1-й элемент массива. Способ 2
+1
+    }
+}
